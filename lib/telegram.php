182c17c99619a3ad3778deaafb01368e38e54926
David Nieto <davidnieto09@gmail.com>
2016-09-27 13:56:12 +0000

    modificando teclado2

--- lib/telegram.php
+++ lib/telegram.php
@@ -58,15 +58,16 @@ class telegram
 				$keyboardButton3 = array(
 					'text' => "depende"
 				);
-				$keyboard = array($keyboardButton1, $keyboardButton2, $keyboardButton3);
-
+				$keyboard = array(
+					'keyboard' => array($keyboardButton1, $keyboardButton2, $keyboardButton3)
+				);
 				$params = array
 				(
 					'chat_id' => $chat_id,
 					'text' => $response,
 					'disable_web_page_preview' => null,
-					'reply_markup' => $keyboard,
-					'reply_to_message_id' => $reply_to_message_id
+					'reply_to_message_id' => $reply_to_message_id,
+					'reply_markup' => $keyboard
 				);
 				$this->request('sendMessage', $params);
 				break;
